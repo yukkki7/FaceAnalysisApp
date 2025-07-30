dbe02a9c6807d21062c8c2bea1eb113ce08fda4f
yukkki7 <yukkichen225@gmail.com>
2025-07-30 14:48:33 +0900

    Finished model inputs set up

--- FaceDetection/FaceDetector.cs
+++ FaceDetection/FaceDetector.cs
@@ -5,7 +5,7 @@ using System.Drawing;
 using Microsoft.ML.OnnxRuntime;
 using Microsoft.ML.OnnxRuntime.Tensors;
 
-"test"
+
 
 namespace FaceAnalysisApp.FaceDetection
 {
@@ -18,15 +18,41 @@ namespace FaceAnalysisApp.FaceDetection
         public FaceDetector(string modelPath)
         {
             session = new InferenceSession(modelPath);
-            """
+            
             Console.WriteLine("[INFO] Model loaded successfully");
-            """
+
         }
 
         public void DetectFaces(string imagePath)
         {
-            Console.WriteLine($"[INFO] Ready to detect faces in image: {imagePath}")
+            Console.WriteLine($"[INFO] Ready to detect faces in image: {imagePath}");
         }
+
+        private float[] Preprocess(Bitmap image)
+        {
+            Bitmap resized = new Bitmap(image, InputWidth, InputHeight);
+            float[] data = new float[InputWidth * InputHeight * 3];
+
+            for (int y = 0; y < InputHeight; y++)
+            {
+                for (int x =0;x < InputWidth; x++)
+                {
+                    int baseIndex = y * InputWidth + y;
+                    Color pixel = resized.GetPixel(x, y);
+                    data[baseIndex + 0 * InputWidth * InputHeight] = pixel.B;
+                    data[baseIndex + 1 * InputWidth * InputHeight] = pixel.G;
+                    data[baseIndex + 2 * InputWidth * InputHeight] = pixel.R;
+                }
+            }
+            return data;
+        }
+
+        //private Dictionary<string, Tensor<float>> RunInference(float[] data)
+        //{
+        //    int dimensions = new int[] { 1, 3, 640, 640};
+        //    var tensor = new DenseTensor<float>(data, dimensions);
+        //    return outputs
+        //}
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Drawing;
+using FaceAnalysisApp.FaceDetection;
+using Microsoft.ML.OnnxRuntime;
+
+class Program
+{
+    static void Main()
+    {
+        var session = new InferenceSession("models/scrfd_500m.onnx");
+        Console.WriteLine("Model inputs:");
+        foreach (var name in session.InputMetadata.Keys)
+        {
+            Console.WriteLine($"  {name}");
+        }
+
+
+        //var baseDir = AppContext.BaseDirectory;
+        //Console.WriteLine($"BaseDirectory: {baseDir}");
+        //Console.WriteLine("Files under BaseDirectory:");
+        //foreach (var file in Directory.GetFiles(baseDir))
+        //    Console.WriteLine("  " + Path.GetFileName(file));
+        //Console.WriteLine("Subdirs under BaseDirectory:");
+        //foreach (var dir in Directory.GetDirectories(baseDir))
+        //{
+        //    Console.WriteLine("  " + Path.GetFileName(dir));
+        //    foreach (var f in Directory.GetFiles(dir))
+        //        Console.WriteLine("    " + Path.GetFileName(f));
+        //}
+
+        //// 停在这里，看看输出后再继续
+        //return;
+    }
+}
